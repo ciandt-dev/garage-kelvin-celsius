050c9e6052dfc958284abf2d9f7f3d710953731a
Travis <travis@travis-ci.org>
2016-05-11 19:31:47 -0300

    Fix for travis deploy.

--- scripts/upload-binary.sh
+++ scripts/upload-binary.sh
@@ -2,23 +2,24 @@
   echo -e "Starting to update gh-pages\n"
 
   #copy data we're interested in to other place
-  cp -R .pioenvs/nodemcuv2/firmware.elf $HOME/build/firmware.elf
+  mkdir -p $HOME/$TRAVIS_BUILD_NUMBER 
+  cp -R .pioenvs/nodemcuv2/firmware.elf $HOME/$TRAVIS_BUILD_NUMBER/firmware.elf
 
   #go to home and setup git
   cd $HOME
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "Travis"
 
+  #remove old clone if exists
+  rm -rf gh-pages
+
   #using token clone gh-pages branch
   git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/ciandt-dev/garage-kelvin-celsius.git  gh-pages > /dev/null
 
   #go into diractory and copy data we're interested in to that directory
-  cd gh-pages
-  mkdir build
-  cd build
-  mkdir $TRAVIS_BUILD_NUMBER
-  cd $TRAVIS_BUILD_NUMBER
-  cp -Rf $HOME/build/* .
+  cd $HOME/gh-pages
+  mkdir -p $HOME/gh-pages/build/$TRAVIS_BUILD_NUMBER
+  cp -Rf $HOME/$TRAVIS_BUILD_NUMBER/* $HOME/gh-pages/build/$TRAVIS_BUILD_NUMBER
 
   #add, commit and push files
   git add -f .
@@ -27,3 +28,6 @@
 
   echo -e "Done.\n"
 #fi
+
+
+ls $HOME/gh-pages/build/$TRAVIS_BUILD_NUMBER
