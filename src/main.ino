b6f2a9f72cec9edb525f26e7e76f1d1c0db71259
Garage <garage@ciandt.com>
2016-09-15 13:35:45 -0300

    Adiciona wifi manager

--- src/main.ino
+++ src/main.ino
@@ -1,9 +1,13 @@
 
 #include <ESP8266WiFi.h>
 #include <Timer.h>
-#include <WiFiClient.h>
+#include <WiFiClient.h>     //https://github.com/esp8266/Arduino
 
+//needed for library
+#include <DNSServer.h>
 #include <ESP8266WebServer.h>
+#include <WiFiManager.h>
+
 
 const char *ssid = "WiFiGaragem";
 const char *password = "senhaGaragem";
@@ -17,25 +21,40 @@ long voteStartTime = 0;
 float hum, temp;
 
 void setup() {
-  delay(200);
+  delay(5000);
   Serial.begin(9600);
   Serial.println();
   Serial.print("Configuring access point...");
   /* You can remove the password parameter if you want the AP to be open. */
-  WiFi.softAP(ssid, password);
+  // WiFi.softAP(ssid, password);
+  //
+  // IPAddress myIP = WiFi.softAPIP();
+  // Serial.print("AP IP address: ");
+  // Serial.println(myIP);
 
-  IPAddress myIP = WiFi.softAPIP();
-  Serial.print("AP IP address: ");
-  Serial.println(myIP);
+  //webserver_setup();
 
-  webserver_setup();
-  dht_setup();
-  remote_setup();
+  WiFiManager wifiManager;
+  wifiManager.setDebugOutput(true);
+  wifiManager.setTimeout(10);
+
+  if(!wifiManager.startConfigPortal("nomeA")){
+    Serial.println("fail to connect");
+    delay(3000);
+    ESP.reset();
+    delay(5000);
+  } else {
+    delay(5000);
+    Serial.println("aew");
+  }
+
+  //dht_setup();
+  //remote_setup();
 
   Serial.println("HTTP server started");
 }
 
 void loop() {
-  t.update();
-  server.handleClient();
+  //t.update();
+  //server.handleClient();
 }
