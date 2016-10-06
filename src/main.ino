a78c4318d712a06e1cdf8a898a0c2a3fab9ccb35
Garage <garage@ciandt.com>
2016-10-06 13:24:57 -0300

    Agora deu certo!

--- src/main.ino
+++ src/main.ino
@@ -21,44 +21,22 @@ long voteStartTime = 0;
 float hum, temp;
 
 void setup() {
-  delay(5000);
-  Serial.begin(9600);
+Serial.begin(9600);
   Serial.println();
   Serial.print("Configuring access point...");
-  /* You can remove the password parameter if you want the AP to be open. */
-  // WiFi.softAP(ssid, password);
-  //
-  // IPAddress myIP = WiFi.softAPIP();
-  // Serial.print("AP IP address: ");
-  // Serial.println(myIP);
 
-  //webserver_setup();
 
-  WiFiManager wifiManager;
-  wifiManager.setDebugOutput(true);
-  wifiManager.setTimeout(10);
+WiFiManager wifiManager;
+//wifiManager.resetSettings();
+wifiManager.autoConnect("ArCondicionado");
+Serial.println("connected...yeey :)");
+webserver_setup();
 
-  // wifiManager.setSTAStaticIPConfig(IPAddress(192,168,4,10), IPAddress(192,168,4,1), IPAddress(255,255,255,0));
-
-  if(!wifiManager.startConfigPortal("nomeA")){
-    Serial.println("fail to connect");
-    delay(3000);
-    ESP.reset();
-    delay(5000);
-  } else {
-    delay(5000);
-    Serial.println("aew");
-    Serial.println("IP do WiFi:");
-    Serial.println(WiFi.localIP());
-  }
-
-  //dht_setup();
-  //remote_setup();
-  // wifiManager.autoConnect("nomeB");
-  Serial.println("HTTP server started");
+dht_setup();
+remote_setup();
 }
 
 void loop() {
-  //t.update();
-  //server.handleClient();
+  t.update();
+  server.handleClient();
 }
